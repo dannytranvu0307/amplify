217a832a008aa1d666dce3b0d8e2cea54455b68f
vu tran <quachvotinh006@gmail.com>
2023-08-08 21:19:34 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  
+  useEffect(() => {
+    // Define the API endpoint URL
+    const apiUrl = 'https://fvg773szqa.execute-api.ap-northeast-1.amazonaws.com/Prod/hello';
+
+    // Make a GET request to the API using Axios
+    axios.get(apiUrl)
+      .then(response => {
+        // Handle the successful response
+        setData(response.data);
+      })
+      .catch(error => {
+        // Handle errors
+        console.error('Error fetching data:', error);
+      });
+  }, [inputValue]);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const HandleSubmitUser = ()=>{
+    const apiUrl = 'https://fvg773szqa.execute-api.ap-northeast-1.amazonaws.com/Prod/hello';
+
+    // Make a GET request to the API using Axios
+    axios.post(apiUrl,{
+      name: inputValue
+    })
+      .then(response => {
+        // Handle the successful response
+        setInputValue("")
+        console.log(response)
+      })
+      .catch(error => {
+        // Handle errors
+        console.error('Error fetching data:', error);
+      });
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='text-8xl'> Data user</h1>
+      {data.users&& data.users.map((user)=>
+      
+        <div className='border p-4'>
+        <span>id: {user.id}</span> 
+        <div> name: {user.name}</div>
+        </div>)}
+        <div >
+        <label htmlFor="inputField">User name </label>
+        <input
+        className='border border-black h-10 '
+          type="text"
+          id="inputField"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+     <button className='bg-sky-600 rounded m-5' onClick={HandleSubmitUser} > Submit User</button>
+       
+      </div>
     </div>
   );
-}
+};
 
 export default App;
